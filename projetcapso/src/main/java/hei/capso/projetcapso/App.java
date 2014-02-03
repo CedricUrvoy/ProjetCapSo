741e520e5fca3fce28d56d8be569e823a9e10e49
CedricUrvoy <cedric.urvoy@hei.fr>
2014-02-03 15:34:17 +0100

    Creation du projet Maven

---
+++ projetcapso/src/main/java/hei/capso/projetcapso/App.java
@@ -0,0 +1,13 @@
+package hei.capso.projetcapso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
