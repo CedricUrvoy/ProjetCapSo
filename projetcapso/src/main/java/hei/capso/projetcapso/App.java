41d50d7d91994938f8cac3c96fd775766b8e6975
CedricUrvoy <cedric.urvoy@hei.fr>
2014-02-07 14:45:36 +0100

    Revert "Maj projet"
    This reverts commit 95a65b385b861ef52cf28bec4bba7070f8454807.

---
+++ projetcapso/src/main/java/hei/capso/projetcapso/App.java
@@ -0,0 +1,13 @@
+package hei.capso.projetcapso;
+
+/**
+ * Hello world! bla bla 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Test GitHub 2" );
+    }
+}
