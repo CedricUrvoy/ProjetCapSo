7f78b2f3d92301435dde6de2c1af82e20f32e61c
MarieFievet <marie.fievet@hei.fr>
2014-04-30 17:35:28 +0200

    quitter groupe ok ( vraiment )

--- projetcapso/src/main/java/hei/capso/projetcapso/controllers/RejoindreGroupeServlet.java
+++ projetcapso/src/main/java/hei/capso/projetcapso/controllers/RejoindreGroupeServlet.java
@@ -1,9 +1,9 @@
 package hei.capso.projetcapso.controllers;
 
 import hei.capso.projetcapso.manager.EleveManager;
-import hei.capso.projetcapso.manager.GroupeManager;
+
 import hei.capso.projetcapso.model.Eleve;
-import hei.capso.projetcapso.model.Groupe;
+
 
 import java.io.IOException;
 
@@ -13,29 +13,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class QuitterGroupeServlet extends HttpServlet{
+public class RejoindreGroupeServlet extends HttpServlet {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 4680923727960935228L;
+	private static final long serialVersionUID = -2684573526927291509L;
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		//QUITTER UN GROUPE
+		//REJOINDRE UN GROUPE
 		HttpSession session = req.getSession();
 		Eleve eleve = (Eleve) session.getAttribute("sessionEleve");
 		req.setAttribute("eleve",eleve);
 		Integer id_Eleve = eleve.getId_Eleve();
-		String nom_groupe = req.getParameter("quitterGroupe");
-		Groupe groupe =GroupeManager.getInstance().chercherGroupeNom(nom_groupe);
-		System.out.print("marie"+groupe);
-		Integer id_Groupe = groupe.getId_Groupe();
 		
-		GroupeManager.getInstance().quitterGroupe(id_Eleve, id_Groupe);
+		
+		
+		EleveManager.getInstance().rejoindreGroupe(id_Eleve, 1);
 		
 		response.sendRedirect("profil");
 	}
-	
 }
